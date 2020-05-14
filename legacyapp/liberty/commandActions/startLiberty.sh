123277556f883233857e6409b7ff05da9ff1507e
Michael Glavassevich <mrglavas@ca.ibm.com>
2020-05-14 15:05:38 -0400

    Adding a container with scripts for starting and stopping Liberty servers.

---
+++ legacyapp/liberty/commandActions/startLiberty.sh
@@ -0,0 +1,25 @@
+###############################################################################
+# Copyright 2020 IBM Corporation
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+###############################################################################
+
+echo Starting Liberty
+
+host=$1
+user=$2
+password=$3
+libertydir=$4
+servername=$5
+
+sshpass -p ''$password'' ssh -o StrictHostKeyChecking=no $user@$host ''sh $libertydir/bin/server start $servername''
